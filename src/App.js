1354d6282fe5a6c664a28766e31e21d36d2e5c38
Kellie Matteson <kelliematteson@yahoo.com>
2021-04-06 08:28:12 -0700

    completed frontend portfolio

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends React.Component {
             <Nav className="ml-auto">
               <Link className="nav-link" to="/">Home</Link>
               <Link className="nav-link" to="/about">About</Link>
-              <Link className="nav-link" to="/contact">Contact</Link>
+              <Link className="nav-link" to="/">Contact</Link>
 
             </Nav>
           </NavBar.Collapse>
